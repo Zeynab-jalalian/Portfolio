2f3b3b290949a78d1f6a0774dde46b570928921f
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-04-11 13:39:47 +0330

    script

--- style-switcher.js
+++ style-switcher.js
@@ -21,3 +21,17 @@ alternateStyles.forEach((style)=>{
     }
 })
 }
+/*light and dark*/
+const dayNight=document.querySelector(".day-night");
+dayNight.addEventListener("click",()=>{
+    dayNight.querySelector("i").classList.toggle('fa-sun');
+    dayNight.querySelector("i").classList.toggle('fa-moon');
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load",()=>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add('fa-sun');
+    }else{
+        dayNight.querySelector("i").classList.add('fa-moon');
+    }
+})
