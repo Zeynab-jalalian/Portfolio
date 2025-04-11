67a2e534373bb78170aa6d9c51152c623c4d19df
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-04-11 18:36:57 +0330

    menu finish

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const navToggler=document.querySelector(".nav-toggler");
 const sidebar=document.querySelector(".aside");
+const navItems=document.querySelectorAll(".nav li");
 navToggler.addEventListener("click",function(){
     sidebar.classList.toggle("open");
     document.querySelector(".nav-toggler i").classList.toggle("fa-times");
+})
+navItems.forEach(link=>{
+    link.addEventListener("click",function(){
+        sidebar.classList.remove("open");
+        document.querySelector(".nav-toggler i").classList.toggle("fa-times");
+    })
 })
